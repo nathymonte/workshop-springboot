ac9660a33d8214a6a3b16dfb67b01ddb2c0bd40c
Nathalia Monteiro <n.monteiro@unifesp.br>
2025-03-16 16:16:30 -0300

    Obtendo usuario por ID

--- src/main/java/com/projetosnath/workshopmongo/resources/UserResource.java
+++ src/main/java/com/projetosnath/workshopmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,12 +21,19 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	//opção alternativa @GetMapping
+	@RequestMapping(method=RequestMethod.GET) //opção alternativa @GetMapping	
 	public ResponseEntity<List<UserDTO>> findAll() {		
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map( x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //opção alternativa @GetMapping	
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {		
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	
 
 }
--- src/main/java/com/projetosnath/workshopmongo/services/UserService.java
+++ src/main/java/com/projetosnath/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.projetosnath.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetosnath.workshopmongo.domain.User;
 import com.projetosnath.workshopmongo.repository.UserRepository;
+import com.projetosnath.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,12 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User não encontrado"));
+		
+				
+	}
+	
 }
