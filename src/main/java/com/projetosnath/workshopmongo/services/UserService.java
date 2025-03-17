65795c8873de28e74ee7bd50a36708d0a06658d4
Nathalia Monteiro <n.monteiro@unifesp.br>
2025-03-17 19:57:03 -0300

    Inserção de usuário com POST

--- src/main/java/com/projetosnath/workshopmongo/services/UserService.java
+++ src/main/java/com/projetosnath/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetosnath.workshopmongo.domain.User;
+import com.projetosnath.workshopmongo.dto.UserDTO;
 import com.projetosnath.workshopmongo.repository.UserRepository;
 import com.projetosnath.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,9 +24,16 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("User não encontrado"));
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("User não encontrado"));		
 				
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
