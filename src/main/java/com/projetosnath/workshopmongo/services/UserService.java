57c960c14e33b2c10bf0a48bfbd450e2c5e9d1eb
Nathalia Monteiro <n.monteiro@unifesp.br>
2025-03-17 21:03:31 -0300

    Método PUT

--- src/main/java/com/projetosnath/workshopmongo/services/UserService.java
+++ src/main/java/com/projetosnath/workshopmongo/services/UserService.java
@@ -37,6 +37,18 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		Optional<User> optionalUser = repo.findById(obj.getId());		
+		User newObj = optionalUser.orElseThrow(() -> new ObjectNotFoundException("User não encontrado"));
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
